8f3e06ed7c565f86e3c6bc9b791842c67fa33f4d
vcani003 <vcani003@fiu.edu>
2018-01-17 15:12:29 -0500

    first commit, ch7

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -0,0 +1,27 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+
+        //create the manager
+        FragmentManager fm = getSupportFragmentManager();
+
+        //fragment_crime.xml is the container
+        //
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+
+    }
+}
