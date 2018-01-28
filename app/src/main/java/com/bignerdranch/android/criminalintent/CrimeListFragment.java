042f1ac0b5aa3d95b73c34cb4bac9f8c508641d5
vcani003 <vcani003@fiu.edu>
2018-01-28 14:22:04 -0500

    added a toast to display when a row is clicked

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -37,7 +38,9 @@ public class CrimeListFragment extends Fragment {
 
 
     //inflates the layout for the list items in the recyclerView
-    private class CrimeHolder extends RecyclerView.ViewHolder{
+    //implements onClickListener for the ViewHolder
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
         private TextView mTitleTextView;
         private TextView mDateTextView;
         private Crime mCrime;
@@ -45,9 +48,16 @@ public class CrimeListFragment extends Fragment {
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent){
 
             super(inflater.inflate(R.layout.list_item_crime, parent, false));
+            itemView.setOnClickListener(this); // override onClick to make it do w.e you want
             mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
             mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
 
+        }
+
+        @Override
+        public void onClick(View view){ // in this case, it will display a toast of the row info
+            Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!",
+                    Toast.LENGTH_SHORT).show();
         }
 
         //bind textViews to show a particular crime
